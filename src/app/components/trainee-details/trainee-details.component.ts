9275cec5a7aa5442e0d6c711f7f459cdad5cf287
YiiSauce <youssefyouss321@gmail.com>
2024-02-09 10:33:10 +0100

    last

--- src/app/components/trainee-details/trainee-details.component.ts
+++ src/app/components/trainee-details/trainee-details.component.ts
@@ -1,32 +1,44 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 import { TraineeService } from '../../services/trainee.service';
 import { Trainee } from '../../models/trainee';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
 import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-trainee-details',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './trainee-details.component.html',
   styleUrl: './trainee-details.component.css'
 })
-export class TraineeDetailsComponent {
-  constructor(public service: TraineeService) {
-    
+export class TraineeDetailsComponent implements OnInit{
+  myForm: FormGroup = new FormGroup({
+    id : new FormControl(0),
+    traineeName : new FormControl(""),
+    age : new FormControl(0),
+    isWorking : new FormControl(false)
+  });
+  constructor( private formBuilder: FormBuilder ,public service: TraineeService) {
+  }
+
+  ngOnInit():void{
+
   }
 
   onSubmit(form: NgForm) {
     this.service.formSubmitted=true;
-    if (form.valid) {      
+    const formData = this.myForm.value;
+    if (this.myForm.valid) {      
       if(this.service.t().id == 0){
         this.insertItem(this.service.t());
-        this.service.resetForm(form);
+        this.myForm.reset();
       }
       else{
         this.updateItem(this.service.t());
-        this.service.resetForm(form);
+        this.myForm.reset();
       }
+    } else {
+      console.error('Form is invalid.');
     }
 }
 insertItem(form: Trainee){
